8fed25f073b61f9585b6be4a2081b279c605c772
Ilmari Åsenbrygg <k1538@student.jamk.fi>
2017-09-06 09:04:30 +0300

    Vk01 added and namespace changed

--- OOdemot/Program.cs
+++ OOdemot/Program.cs
@@ -1,7 +1,9 @@
-﻿using JAMK.IT; //helpottaa meidän tekemien luokkien löytämistä
+﻿// TTOS0200 Olio-ohjelmointi viikkotehtävät
+//Ilmari Åsenbrygg
+using JAMK.IT; //helpottaa meidän tekemien luokkien löytämistä
 using System;
 
-namespace OOdemot
+namespace JAMK.IT
 {
   class Program
   {
