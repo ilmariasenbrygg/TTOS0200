fa35cc22e09c41b151fbde251e8693757c635751
Ilmari Åsenbrygg <k1538@student.jamk.fi>
2017-10-21 18:12:39 +0300

    Tehtävän 6.4 alku

---
+++ Lab6.4/Pelaaja.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6._4
+{
+    class Pelaaja
+    {
+        public string Nimi { get; set; }
+        public string Pelipaikka { get; set; }
+        public string Kätisyys { get; set; }
+        public int Numero { get; set; }
+
+        public Pelaaja()
+        {
+        }
+        public Pelaaja(string nimi, string pelipaikka, string kätisyys, int numero)
+        {
+            Nimi = nimi;
+            Pelipaikka = pelipaikka;
+            Kätisyys = kätisyys;
+            Numero = numero;
+        }
+        public override string ToString()
+        {
+            return Nimi + " pelipaikka " + Pelipaikka + " ,kätisyys" + Kätisyys + " ja pelinumero" + Numero;
+        }
+    }
+}
--- Lab6.4/Program.cs
+++ Lab6.4/Program.cs
@@ -10,6 +10,20 @@ namespace Lab6._4
     {
         static void Main(string[] args)
         {
+            Joukkue Tps = new Joukkue();
+            Tps.Name = "TPS";
+            Tps.pelaajat.Add(new Pelaaja("Teppo Numminen", "Hyökkääjä", "Right", 23));
+            Tps.pelaajat.Add(new Pelaaja("Sami Joku", "Puolustaja", "Right", 22));
+            Tps.pelaajat.Add(new Pelaaja("Esko Aho", "Hyökkääjä", "Right", 9));
+
+            Console.WriteLine("Anna pelaajan nimi:");
+            string Joku = Console.ReadLine();
+            Pelaaja o = new Pelaaja();
+            o.Nimi = Joku;
+
+            
+
+            
         }
     }
 }
