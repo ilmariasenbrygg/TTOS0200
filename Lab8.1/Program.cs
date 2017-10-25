ae1d5f3b5ec181b1b53231894454b0b8be495905
Ilmari Åsenbrygg <k1538@student.jamk.fi>
2017-10-25 10:49:25 +0300

    lab8.1 ja 8.2 alku

---
+++ Lab8.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nimi;
+            string mydocpath = @"D:\K1538\Visualstudio\Tekstit\";
+            System.IO.StreamWriter outputFile = new System.IO.StreamWriter(mydocpath + @"\test.txt");
+            do
+            {
+                Console.WriteLine("Anna Tiedostoon tallennettavia nimiä: ");
+                nimi = Console.ReadLine();
+                outputFile.WriteLine(nimi);
+            } while (nimi.Length != 0);
+            outputFile.Close();
+            
+            try
+            {
+                string text = System.IO.File.ReadAllText(mydocpath + @"\test.txt");
+                System.Console.WriteLine("Contents of test.txt:\n" + text);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found (FileNotFoundException)");
+            }
+        }
+    }
+}
---
+++ lab8.2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rivit = 0;
+            int nimet = 0;
+            int j = 0;
+            string nimi;
+            string[] nimia = new string[rivit];
+            try
+            {
+                string polku = @"D:\K1538\Visualstudio\Tekstit\nimet.txt";
+                if (!File.Exists(polku))
+                {
+                    Console.WriteLine("Tiedosto on jo olemassa");
+                }
+                else
+                {
+                    string[] lines = System.IO.File.ReadAllLines(polku);
+                    foreach (string line in lines)
+                    {
+                        Console.WriteLine(line);
+                        rivit++;
+                        nimia[j] = line;
+                        j++;
+                         
+                    }
+                    for (int i = 0; i < nimia.Length; i++)
+                    {
+                        if(nimia[i] = )
+                    }
+                }
+
+            }
+            catch { }
+        }
+    }
+}
