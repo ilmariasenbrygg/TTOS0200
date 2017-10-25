05d21e211057df12512f0179598d73f1f7d32514
Ilmari Åsenbrygg <k1538@student.jamk.fi>
2017-10-25 10:54:38 +0300

    dictionary

--- lab8.2/Program.cs
+++ lab8.2/Program.cs
@@ -39,6 +39,26 @@ namespace lab8._2
                         if(nimia[i] = )
                     }
                 }
+                //private static IDictionary<string, int> ParseNameFile(string filename)
+                //{
+                //    var names = new Dictionary<string, int>();
+                //    using (var reader = new StreamReader(filename))
+                //    {
+                //        var line = reader.ReadLine();
+                //        while (line != null)
+                //        {
+                //            if (names.ContainsKey(line))
+                //            {
+                //                names[line]++;
+                //            }
+                //            else
+                //            {
+                //                names.Add(line, 1);
+                //            }
+                //            line = reader.ReadLine();
+                //        }
+                //    }
+                //}
 
             }
             catch { }
