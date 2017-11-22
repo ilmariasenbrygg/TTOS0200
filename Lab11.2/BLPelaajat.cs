5c4ee3bed76d6ac445a898f5c99bfd471bb1b89a
Ilmari Åsenbrygg <k1538@student.jamk.fi>
2017-11-22 06:54:57 +0200

    Lab11.2

---
+++ Lab11.2/BLPelaajat.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11._2
+{
+    public class Pelaajat
+    {
+        public string Etunimi { get; set; }
+        public string Sukunimi { get; set; }
+        public string Pelipaikka { get; set; }
+        public string Katisyys { get; set; }
+        public double Numero { get; set; }
+
+        public Pelaajat(string etunimi, string sukunimi, string pelipaikka, string katisyys, int numero)
+        {
+            Etunimi = etunimi;
+            Sukunimi = sukunimi;
+            Pelipaikka = pelipaikka;
+            Katisyys = katisyys;
+            Numero = numero;
+
+        }
+    }
+    public static class Joukkue
+    {
+        public static List<Pelaajat> HaePelaajat()
+        {
+            List < Pelaajat > Pelaaja = new List<Pelaajat>();
+
+            Pelaaja.Add(new Pelaajat("Jaska", "Jokunen", "Pakki", "Left", 8));
+            Pelaaja.Add(new Pelaajat("Jaska", "Jokunen", "Pakki", "Left", 8));
+            Pelaaja.Add(new Pelaajat("Jaska", "Jokunen", "Pakki", "Left", 8));
+            Pelaaja.Add(new Pelaajat("Jaska", "Jokunen", "Pakki", "Left", 8));
+            Pelaaja.Add(new Pelaajat("Jaska", "Jokunen", "Pakki", "Left", 8));
+            Pelaaja.Add(new Pelaajat("Jaska", "Jokunen", "Pakki", "Left", 8));
+            Pelaaja.Add(new Pelaajat("Jaska", "Jokunen", "Pakki", "Left", 8));
+            Pelaaja.Add(new Pelaajat("Jaska", "Jokunen", "Pakki", "Left", 8));
+            Pelaaja.Add(new Pelaajat("Jaska", "Jokunen", "Pakki", "Left", 8));
+            Pelaaja.Add(new Pelaajat("Jaska", "Jokunen", "Pakki", "Left", 8));
+
+            return Pelaaja;
+        }
+    }
+}
